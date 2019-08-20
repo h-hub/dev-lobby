448a90dad7653588c672c7140d34db623ae27166
h-hub <harsha.kj89@gmail.com>
2019-08-21 02:16:21 +0800

    add bootstrap support

--- tweetSearch/views.py
+++ tweetSearch/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+import datetime
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the tweetSearch index.")
+    today = datetime.datetime.now().date()
+    return render(request, "home.html", {"today" : today})
