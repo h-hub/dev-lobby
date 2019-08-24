4cabebfbec38035df9fb5809a40d06899c07b536
h-hub <harsha.kj89@gmail.com>
2019-08-25 02:28:48 +0800

    adding user management module with views

--- WebAppProject/urls.py
+++ WebAppProject/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('tweetSearch/', include('tweetSearch.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('userManagement.urls')),
 ]
