abc188fe60486b5b53d8d33af9842f55045a2945
h-hub <harsha.kj89@gmail.com>
2019-08-25 17:17:57 +0800

    add models and views for user registration

--- userManagement/models.py
+++ userManagement/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils import six
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email_confirmed = models.BooleanField(default=False)
+
+
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
+
+
+class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
+    def _make_hash_value(self, user, timestamp):
+        return (
+                six.text_type(user.pk) + six.text_type(timestamp) +
+                six.text_type(user.profile.email_confirmed)
+        )
+
+
+account_activation_token = AccountActivationTokenGenerator()
