703cceb07ab4420a326c6d1ee2b0babdae4ea655
h-hub <harsha.kj89@gmail.com>
2019-09-10 22:58:13 +0800

    bug fix and read me edit

--- devLobbyWebApplication/settings.py
+++ devLobbyWebApplication/settings.py
@@ -147,8 +147,6 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 EMAIL_HOST = 'localhost'
 EMAIL_PORT = 1025
